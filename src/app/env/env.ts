47a18d55a1cc15fdafa459d0c89acf63a8c09c0b
nexaItTeam <itsupport@arctechz.com>
2023-08-09 16:15:58 +0530

    first commit

---
+++ src/app/env/env.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    apiBase:"",  
+PROPERTY_ENDPOINT:"https://nexa-capital.onrender.com/api/property/",
+ENQUIRY_ENDPOINT:"https://nexa-capital.onrender.com/api/enquery/"
+
+
+
+
+
+}
