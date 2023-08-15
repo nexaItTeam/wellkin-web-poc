48a91e37219d810bc52efeddfe5b830b1a190b6f
nexaItTeam <itsupport@arctechz.com>
2023-08-15 15:31:52 +0530

    dist

--- src/app/env/env.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    apiBase: "",
-    PROPERTY_ENDPOINT: "https://wellkin-backend-dev.azurewebsites.net/api/property/",
-    ENQUIRY_ENDPOINT: "https://wellkin-backend-dev.azurewebsites.net/api/enquery/"
-}
--- src/app/shared/config.ts
+++
@@ -1,8 +0,0 @@
-export const ACCESS_TOKEN_KEY: string = 'Authorization';
-export enum CustomEvents {
-    UNAUTHORISED,
-}
-export enum RestEnds { 
-    getAllProperty = "/property/getAll"
-
-}
